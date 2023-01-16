0fccb0b15f342eade39f737db624806a1432a93d
waheednazeer <waheed.nazeer@hotmail.com>
2023-01-16 19:41:10 +0500

    js

--- week03/scripts/lazyload.js
+++ week03/scripts/lazyload.js
@@ -7,15 +7,3 @@ document.getElementById("currentyear").innerHTML = currentYear;
 // Get Last Modified Date
 var lastModifiedDate = document.lastModified;
 document.getElementById("lastmodified").innerHTML = lastModifiedDate;
-
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-
-// Add a simple arrow function that listens for the <li> hamburger button click event.
-// When clicked, the <ul class="navigation">'s class list toggle'
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-function darkModeToggle() {
-    const mainArea = document.querySelector("main");
-    mainArea.classList.toggle("dark");
-  }
