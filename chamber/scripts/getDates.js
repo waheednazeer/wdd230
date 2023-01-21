958518904ddca63a9184d1ced4a5470962ffd9eb
waheednazeer <waheed.nazeer@hotmail.com>
2023-01-21 13:14:43 +0500

    discover.html

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -7,3 +7,29 @@ document.getElementById("currentyear").innerHTML = currentYear;
 // Get Last Modified Date
 var lastModifiedDate = document.lastModified;
 document.getElementById("lastmodified").innerHTML = lastModifiedDate;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+function darkModeToggle() {
+    const mainArea = document.querySelector("main");
+    mainArea.classList.toggle("dark");
+  }
+  // for visitor counter
+let counterRecord = document.querySelector(".counter");
+let visitCount = Number(window.localStorage.getItem("visits-ls"));
+
+// Check if page_view entry is present
+if (visitCount !== 0) {
+  counterRecord.innerHTML = visitCount;
+  
+} 
+else{
+  counterRecord.innerHTML = "Welcome for your first visit!";
+}
+visitCount+=1;
+localStorage.setItem("visits-ls", visitCount);
