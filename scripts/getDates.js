4355bbb077a88aad5d19dc10bff41861ee665006
waheednazeer <waheed.nazeer@hotmail.com>
2023-01-24 00:33:19 +0500

    GG

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -33,3 +33,46 @@ else{
 }
 visitCount+=1;
 localStorage.setItem("visits-ls", visitCount);
+
+//----------------------Form & Table--------------------------
+function myForm(){
+const myTable = document.querySelector('table');
+let senderName=document.getElementById('name').value;
+let senderEmail=document.getElementById('email').value;
+let senderRating=document.getElementById('rating').value;
+let senderUsername=document.getElementById('rating').value;
+let senderUsername2=document.getElementById('username2').value;
+if (senderName==''){
+window.alert("Full Name can not be kept empty!");
+}
+
+if (senderUsername.length <6 || senderUsername.length >13){
+  window.alert("Phrase must be between 5 and 12!");
+  }
+ 
+if (senderUsername!==senderUsername2){
+
+    window.alert("Please enter same phrase!");
+  }
+document.getElementById('filled-name').innerHTML=senderName;
+document.getElementById('filled-email').innerHTML=senderEmail;
+document.getElementById('filled-rating').innerHTML=senderRating;
+document.getElementById('filled-username').innerHTML=senderUsername;
+myTable.classList.toggle("mytable");
+}
+
+//------------------rating sliding bar---------------------
+const rating = document.getElementById("rating");
+const rangevalue = document.getElementById("r");
+
+function displayRatingValue() {
+    rating.innerHTML = rangevalue.value;
+    console.log(rangevalue.value);
+    if (rangevalue.value>5){
+      rangevalue.classList.toggle("myratings");
+
+    }
+}
+
+rangevalue.addEventListener('change', displayRatingValue);
+rangevalue.addEventListener('input', displayRatingValue);
