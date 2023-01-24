f11147633cb4dd33c4109a93d6737c1b1295f4cd
waheednazeer <waheed.nazeer@hotmail.com>
2023-01-24 22:29:06 +0500

    form

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -36,32 +36,55 @@ localStorage.setItem("visits-ls", visitCount);
 
 //----------------------Form & Table--------------------------
 function myForm(){
-
+let submittedForm=document.querySelector('table');
 let senderName=document.getElementById('name').value;
 let senderEmail=document.getElementById('email').value;
-let senderRating=document.getElementById('rating').value;
+let senderRating=document.getElementById('r').value;
 let senderUsername=document.getElementById('username').value;
 let senderUsername2=document.getElementById('username2').value;
+let senderComments=document.getElementById('comments').value;
 
+
+if (senderRating < 6){
+  document.getElementById('filled-rating').style.color="red";
+}
+else{
+  document.getElementById('filled-rating').style.color="blue";
+}
+
+if(senderName == ''){
+    window.alert("Name can not be kept empty!");
+  }
+  else if (senderEmail == ''){
+    window.alert("Email can not be kept empty!");
+  }
+  else if (senderUsername.length<5 || senderUsername.length >12){
+    window.alert("Phrase muste be between 5 & 12 digits!");
+  }
+  else if (senderUsername !== senderUsername2){
+    window.alert("Re-enter phrase muste be same!");
+  }
+  else if (senderComments.length <5){
+    window.alert("Please write one sentence in Message box!");
+  }
+  else{
 document.getElementById('filled-name').innerHTML=senderName;
 document.getElementById('filled-email').innerHTML=senderEmail;
 document.getElementById('filled-rating').innerHTML=senderRating;
 document.getElementById('filled-username').innerHTML=senderUsername;
-
+submittedForm.classList.toggle("mytable");
+  }
 }
 
 //------------------rating sliding bar---------------------
 const rating = document.getElementById("rating");
 const rangevalue = document.getElementById("r");
-/*
+
 function displayRatingValue() {
     rating.innerHTML = rangevalue.value;
     console.log(rangevalue.value);
-    if (rangevalue.value>5){
-      rangevalue.classList.toggle("myratings");
-
-    }
+    
 }
 
 rangevalue.addEventListener('change', displayRatingValue);
-rangevalue.addEventListener('input', displayRatingValue);*/
+rangevalue.addEventListener('input', displayRatingValue);
