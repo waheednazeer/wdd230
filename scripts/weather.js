2179d9c3d986f728327fdf47e0be97c428cd16fd
waheednazeer <waheed.nazeer@hotmail.com>
2023-01-30 18:35:56 +0500

    weather in portal

---
+++ scripts/weather.js
@@ -0,0 +1,34 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Islamabad,pakistan&APPID=7c4be2e672b7473cb72ef14c53adfb41';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+  function displayResults(data) {
+    let temp=data.main.temp;
+    let cDegTemp=Math.floor(temp-273.15);
+   console.log(data);
+    currentTemp.innerHTML = `${cDegTemp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    console.log(data.weather[0].icon);
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    
+  }
