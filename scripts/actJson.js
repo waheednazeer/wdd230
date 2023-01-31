e9565997918f48805328c59ef4501e70793bfd72
waheednazeer <waheed.nazeer@hotmail.com>
2023-01-31 11:17:17 +0500

    index

--- scripts/actJson.js
+++ scripts/actJson.js
@@ -3,6 +3,7 @@ const section=document.querySelector('#jweek01');
 const jweek02=document.querySelector('#jweek02');
 const jweek03=document.querySelector('#jweek03');
 const jweek04=document.querySelector('#jweek04');
+const jweek05=document.querySelector('#jweek05');
 
 async function jsonFetch() {
    
@@ -60,5 +61,17 @@ async function jsonFetch() {
             weekLink.appendChild(link);
             jweek04.appendChild(weekLink);
                
-         });              
+         });
+         data.week05.forEach((week) =>{
+            let text= "  |";
+            let subset=week.substr(7);
+            let weekLink=document.createElement('a');
+            let link = document.createTextNode(subset.substr(0, subset.length-5).concat(text));
+            weekLink.href = week;
+                
+            weekLink.appendChild(link);
+            jweek05.appendChild(weekLink);
+               
+         });
+                      
   }
