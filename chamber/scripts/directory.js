c464180c108608d3897bbb372af3675aee50df8e
waheednazeer <waheed.nazeer@hotmail.com>
2023-02-03 21:00:35 +0500

    yy

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -30,7 +30,7 @@ async function getProphetData() {
 		logo.setAttribute('src', member.logo);
 		logo.setAttribute('alt', member.name); // fill in the blank
 		logo.setAttribute('loading', 'lazy');
-		logo.setAttribute('width', 'auto');
+		logo.setAttribute('width', '120px');
 		logo.setAttribute('height', '75px');
 		
 		
