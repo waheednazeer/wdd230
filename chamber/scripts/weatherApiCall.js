e9b7ef704a22311eb7186659acbcafd701d1bfbf
waheednazeer <waheed.nazeer@hotmail.com>
2023-02-07 07:06:13 +0500

    t

--- chamber/scripts/weatherApiCall.js
+++ chamber/scripts/weatherApiCall.js
@@ -3,6 +3,7 @@ let results=null;
 async function fetchAPI(api){
     const response=await fetch(api);
     const data=await response.json();
+    console.log(data);
     displayResults(data);
 }
 
@@ -26,24 +27,24 @@ function displayResults(data){
     dayNo+=1;
     console.log(dayNames[dayNo]);
     
-    let dayN1=Math.floor(data.list[10].main.temp);
+    let dayN1=Math.floor(data.list[8].main.temp);
     document.querySelector('#dayN1').innerHTML=`${dayNames[dayNo]}: ${dayN1} &degC`;
 
     let imgN1=document.querySelector('#iconN1');
-    let iconN1= `https://openweathermap.org/img/w/${data.list[10].weather[0].icon}.png`;
+    let iconN1= `https://openweathermap.org/img/w/${data.list[8].weather[0].icon}.png`;
     imgN1.setAttribute('src', iconN1);
    
-    document.querySelector('#desN1').innerHTML= data.list[10].weather[0].description;
+    document.querySelector('#desN1').innerHTML= data.list[8].weather[0].description;
 
     //----------------2nd Day weather----------------------
     dayNo+=1;
-    let dayN2=Math.floor(data.list[18].main.temp);
+    let dayN2=Math.floor(data.list[16].main.temp);
     document.querySelector('#dayN2').innerHTML=`${dayNames[dayNo]}: ${dayN2} &degC`;
 
     let imgN2=document.querySelector('#iconN2');
-    let iconN2= `https://openweathermap.org/img/w/${data.list[18].weather[0].icon}.png`;
+    let iconN2= `https://openweathermap.org/img/w/${data.list[16].weather[0].icon}.png`;
     imgN2.setAttribute('src', iconN2);
-    document.querySelector('#desN2').innerHTML=data.list[18].weather[0].description;
+    document.querySelector('#desN2').innerHTML=data.list[16].weather[0].description;
     
 }
 getWeather();
